bc1f3092891d68e01229a187c2e250418cbd49c0
Ze40 <miko.r4r@gmail.com>
2025-03-27 11:51:32 +0300

    chore: create prisma-schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { IS_DEV_ENV } from './libs/common/utils/is-dev.utils';
+import { AuthModule } from './auth/auth.module';
+import { IS_DEV_ENV } from './libs/common/utils/is-dev.util';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -9,6 +12,9 @@ import { IS_DEV_ENV } from './libs/common/utils/is-dev.utils';
       ignoreEnvFile: !IS_DEV_ENV,
       isGlobal: true,
     }),
+    PrismaModule,
+    AuthModule,
+    UserModule,
   ],
 })
 export class AppModule {}
